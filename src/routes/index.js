16cb36ffe68d5aea106f46c515be8854a6a0d589
Unmukt <unmuktjain.rera@gmail.com>
2026-07-10 18:29:15 +0530

    db add

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+// Health Check
 router.get("/", (req, res) => {
     res.json({
         success: true,
         message: "CAC API v1"
     });
 });
+
+// Feature Routes
+router.use("/upload", require("./upload.routes"));
 
 module.exports = router;
